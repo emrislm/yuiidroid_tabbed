49219bb053a127f3769e9435ccc5afec25bc4e45
emiri <60814488+emrislm@users.noreply.github.com>
2022-01-18 15:47:40 +0100

    added top mangas

--- app/src/main/java/com/emrislm/yuiidroid/HomeFragment.java
+++ app/src/main/java/com/emrislm/yuiidroid/HomeFragment.java
@@ -29,10 +29,16 @@ public class HomeFragment extends Fragment {
     private AnimeTop tempTopAnime;
     private ArrayList<AnimeTop> topAnimeList;
 
+    private String URL_STRING_MANGA = "https://api.jikan.moe/v3/top/manga/1/bypopularity";
+    private MangaTop tempTopManga;
+    private ArrayList<MangaTop> topMangaList;
+
     // define variables for the widgets
     private RecyclerView listView_TopAnimesListView;
-    //private RecyclerView listView_TopMangasListView;
     private AdapterAnime adapterAnime;
+
+    private RecyclerView listView_TopMangasListView;
+    private AdapterManga adapterManga;
 
     private static final String TAG = "HomeFragment";
 
@@ -49,13 +55,13 @@ public class HomeFragment extends Fragment {
 
         // get references to the widgets
         listView_TopAnimesListView = (RecyclerView) view.findViewById(R.id.ListView_topAnimesListView);
-        //listView_TopMangasListView = (RecyclerView) view.findViewById(R.id.ListView_topMangasListView);
+        listView_TopMangasListView = (RecyclerView) view.findViewById(R.id.ListView_topMangasListView);
 
         new getTopFromSearch().start();
         return view;
     }
 
-    public void updateDisplay() {
+    public void updateDisplay_ANIME() {
         if (topAnimeList == null) {
             Log.d("dinges", "ERR: arr is leeg?");
             return;
@@ -73,7 +79,7 @@ public class HomeFragment extends Fragment {
 
         // create and set the adapter
         adapterAnime = new AdapterAnime(getActivity(), titles, imgurls);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2, RecyclerView.VERTICAL, false);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3, RecyclerView.VERTICAL, false);
 
         listView_TopAnimesListView.setLayoutManager(gridLayoutManager);
         listView_TopAnimesListView.setAdapter(adapterAnime);
@@ -81,7 +87,33 @@ public class HomeFragment extends Fragment {
         Log.d("dinges", "Feed displayed");
     }
 
-    public void getTopResults_anime() {
+    public void updateDisplay_MANGA() {
+        if (topMangaList == null) {
+            Log.d("dinges", "ERR: arr is leeg?");
+            return;
+        }
+
+        // create List objects
+        ArrayList<String> titles = new ArrayList<String>();
+        for (MangaTop manga : topMangaList) {
+            titles.add(manga.getTitle());
+        }
+        ArrayList<String> imgurls = new ArrayList<String>();
+        for (MangaTop manga : topMangaList) {
+            imgurls.add(manga.getImage_url());
+        }
+
+        // create and set the adapter
+        adapterManga = new AdapterManga(getActivity(), titles, imgurls);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3, RecyclerView.VERTICAL, false);
+
+        listView_TopMangasListView.setLayoutManager(gridLayoutManager);
+        listView_TopMangasListView.setAdapter(adapterManga);
+
+        Log.d("dinges", "Feed displayed");
+    }
+
+    public void getTopResults_ANIME() {
         HttpHandler sh = new HttpHandler();
         String jsonStr = sh.makeServiceCall(URL_STRING_ANIME);
 
@@ -92,7 +124,7 @@ public class HomeFragment extends Fragment {
                 JSONObject jsonObj = new JSONObject(jsonStr);
                 JSONArray results  = jsonObj.getJSONArray("top");
 
-                for (int i = 0; i < 5; i++) {
+                for (int i = 0; i < 9; i++) {
                     tempTopAnime = new AnimeTop();
                     JSONObject result = results.getJSONObject(i);
 
@@ -102,7 +134,6 @@ public class HomeFragment extends Fragment {
                     tempTopAnime.setUrl(result.getString("url"));
                     tempTopAnime.setImage_url(result.getString("image_url"));
                     tempTopAnime.setType(result.getString("type"));
-                    //tempTopAnime.setEpisodes(result.getInt("episodes"));
                     tempTopAnime.setStart_date(result.getString("start_date"));
                     tempTopAnime.setEnd_date(result.getString("end_date"));
                     tempTopAnime.setMembers(result.getInt("members"));
@@ -112,10 +143,43 @@ public class HomeFragment extends Fragment {
                 }
 
                 Log.d("dinges", "tempanime is gevuld");
-                // check of alle anime binnen zijn
-//                for (AnimeTop a: topAnimeList) {
-//                    Log.d("dinges", a.getTitle());
-//                }
+            }
+            catch (JSONException e) {
+                Log.d("dinges", e.toString());
+            }
+        }
+    }
+
+    public void getTopResults_MANGA() {
+        HttpHandler sh = new HttpHandler();
+        String jsonStr = sh.makeServiceCall(URL_STRING_MANGA);
+
+        topMangaList = new ArrayList<>();
+
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                JSONArray results  = jsonObj.getJSONArray("top");
+
+                for (int i = 0; i < 9; i++) {
+                    tempTopManga = new MangaTop();
+                    JSONObject result = results.getJSONObject(i);
+
+                    tempTopManga.setMal_id(result.getInt("mal_id"));
+                    tempTopManga.setRank(result.getInt("rank"));
+                    tempTopManga.setTitle(result.getString("title"));
+                    tempTopManga.setUrl(result.getString("url"));
+                    tempTopManga.setImage_url(result.getString("image_url"));
+                    tempTopManga.setType(result.getString("type"));
+                    tempTopManga.setStart_date(result.getString("start_date"));
+                    tempTopManga.setEnd_date(result.getString("end_date"));
+                    tempTopManga.setMembers(result.getInt("members"));
+                    tempTopManga.setScore(result.getDouble("score"));
+
+                    topMangaList.add(tempTopManga);
+                }
+
+                Log.d("dinges", "tempmanga is gevuld");
             }
             catch (JSONException e) {
                 Log.d("dinges", e.toString());
@@ -128,14 +192,16 @@ public class HomeFragment extends Fragment {
         public void run() {
 
             //loop the json + fill arraylist with anime
-            getTopResults_anime();
+            getTopResults_ANIME();
+            getTopResults_MANGA();
 
             if (isAdded()) {
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         Log.d("dinges", "runOnUiThread");
-                        HomeFragment.this.updateDisplay();
+                        HomeFragment.this.updateDisplay_ANIME();
+                        HomeFragment.this.updateDisplay_MANGA();
                     }
                 });
             }
