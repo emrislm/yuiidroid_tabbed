79f4385c29da33439736204b837271331cd950ef
emiri <60814488+emrislm@users.noreply.github.com>
2022-01-18 15:22:30 +0100

    added a new homescreen + top 5 anime

---
+++ app/src/main/java/com/emrislm/yuiidroid/AnimeTop.java
@@ -0,0 +1,49 @@
+package com.emrislm.yuiidroid;
+
+public class AnimeTop {
+
+    private int mal_id;
+    private int rank;
+    private String title;
+    private String url;
+    private String image_url;
+    private String type;
+    private int episodes;
+    private String start_date;
+    private String end_date;
+    private int members;
+    private double score;
+
+    public int getMal_id() { return mal_id; }
+    public void setMal_id(int mal_id) { this.mal_id = mal_id; }
+
+    public int getRank() { return rank; }
+    public void setRank(int rank) { this.rank = rank; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    public String getImage_url() { return image_url; }
+    public void setImage_url(String image_url) { this.image_url = image_url; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public int getEpisodes() { return episodes; }
+    public void setEpisodes(int episodes) { this.episodes = episodes; }
+
+    public String getStart_date() { return start_date; }
+    public void setStart_date(String start_date) { this.start_date = start_date; }
+
+    public String getEnd_date() { return end_date; }
+    public void setEnd_date(String end_date) { this.end_date = end_date; }
+
+    public int getMembers() { return members; }
+    public void setMembers(int members) { this.members = members; }
+
+    public double getScore() { return score; }
+    public void setScore(double score) { this.score = score; }
+}
--- app/src/main/java/com/emrislm/yuiidroid/HomeFragment.java
+++ app/src/main/java/com/emrislm/yuiidroid/HomeFragment.java
@@ -14,17 +14,31 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    private String URL_STRING_ANIME = "https://api.jikan.moe/v3/top/anime/1/bypopularity";
+    private AnimeTop tempTopAnime;
+    private ArrayList<AnimeTop> topAnimeList;
+
     // define variables for the widgets
-    private TextView home;
+    private RecyclerView listView_TopAnimesListView;
+    //private RecyclerView listView_TopMangasListView;
+    private AdapterAnime adapterAnime;
+
+    private static final String TAG = "HomeFragment";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 
@@ -33,8 +47,98 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        home = view.findViewById(R.id.textView69);
+        // get references to the widgets
+        listView_TopAnimesListView = (RecyclerView) view.findViewById(R.id.ListView_topAnimesListView);
+        //listView_TopMangasListView = (RecyclerView) view.findViewById(R.id.ListView_topMangasListView);
 
+        new getTopFromSearch().start();
         return view;
+    }
+
+    public void updateDisplay() {
+        if (topAnimeList == null) {
+            Log.d("dinges", "ERR: arr is leeg?");
+            return;
+        }
+
+        // create List objects
+        ArrayList<String> titles = new ArrayList<String>();
+        for (AnimeTop anime : topAnimeList) {
+            titles.add(anime.getTitle());
+        }
+        ArrayList<String> imgurls = new ArrayList<String>();
+        for (AnimeTop anime : topAnimeList) {
+            imgurls.add(anime.getImage_url());
+        }
+
+        // create and set the adapter
+        adapterAnime = new AdapterAnime(getActivity(), titles, imgurls);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2, RecyclerView.VERTICAL, false);
+
+        listView_TopAnimesListView.setLayoutManager(gridLayoutManager);
+        listView_TopAnimesListView.setAdapter(adapterAnime);
+
+        Log.d("dinges", "Feed displayed");
+    }
+
+    public void getTopResults_anime() {
+        HttpHandler sh = new HttpHandler();
+        String jsonStr = sh.makeServiceCall(URL_STRING_ANIME);
+
+        topAnimeList = new ArrayList<>();
+
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                JSONArray results  = jsonObj.getJSONArray("top");
+
+                for (int i = 0; i < 5; i++) {
+                    tempTopAnime = new AnimeTop();
+                    JSONObject result = results.getJSONObject(i);
+
+                    tempTopAnime.setMal_id(result.getInt("mal_id"));
+                    tempTopAnime.setRank(result.getInt("rank"));
+                    tempTopAnime.setTitle(result.getString("title"));
+                    tempTopAnime.setUrl(result.getString("url"));
+                    tempTopAnime.setImage_url(result.getString("image_url"));
+                    tempTopAnime.setType(result.getString("type"));
+                    //tempTopAnime.setEpisodes(result.getInt("episodes"));
+                    tempTopAnime.setStart_date(result.getString("start_date"));
+                    tempTopAnime.setEnd_date(result.getString("end_date"));
+                    tempTopAnime.setMembers(result.getInt("members"));
+                    tempTopAnime.setScore(result.getDouble("score"));
+
+                    topAnimeList.add(tempTopAnime);
+                }
+
+                Log.d("dinges", "tempanime is gevuld");
+                // check of alle anime binnen zijn
+//                for (AnimeTop a: topAnimeList) {
+//                    Log.d("dinges", a.getTitle());
+//                }
+            }
+            catch (JSONException e) {
+                Log.d("dinges", e.toString());
+            }
+        }
+    }
+
+    class getTopFromSearch extends Thread {
+        @Override
+        public void run() {
+
+            //loop the json + fill arraylist with anime
+            getTopResults_anime();
+
+            if (isAdded()) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("dinges", "runOnUiThread");
+                        HomeFragment.this.updateDisplay();
+                    }
+                });
+            }
+        }
     }
 }
